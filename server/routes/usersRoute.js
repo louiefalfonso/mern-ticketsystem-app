2d9b1eed8c6f443e3a9f17c8a4ebbf61afb66d66
Louie Alfonso <79123140+louiefalfonso@users.noreply.github.com>
2024-08-12 21:25:13 +0100

    Update usersRoute.js

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,21 +1,14 @@
 import express from "express";
-import { protectRoute, isAdminRoute } from "../middlewares/authMiddleware.js";
-import {
-  loginUser,
-  logoutUser,
-  registerUser,
-  getAllUsers,
-  updateUser,
-  deleteUser,
-} from "../controllers/userControllers.js";
+import { protectRoute } from "../middlewares/authMiddleware.js";
+import { loginUser, logoutUser, registerUser, getAllUsers, updateUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/register",registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 router.get("/users", getAllUsers);
-router.put("/profile", updateUser);
-router.delete("/delete/:id",deleteUser);
+router.put("/profile", protectRoute, updateUser);
+router.post("/register", registerUser);
 
 export default router;
