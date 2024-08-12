2ba48aa581e97447697f3daab036b8775a384dc3
Louie Alfonso <79123140+louiefalfonso@users.noreply.github.com>
2024-08-12 21:08:19 +0100

    Update usersRoute.js

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -11,11 +11,11 @@ import {
 
 const router = express.Router();
 
-router.post("/register", protectRoute, registerUser);
+router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
-router.get("/users", protectRoute, isAdminRoute, getAllUsers);
-router.put("/profile", protectRoute,isAdminRoute, updateUser);
-router.delete("/delete/:id", protectRoute, isAdminRoute, deleteUser);
+router.get("/users", getAllUsers);
+router.put("/profile", updateUser);
+router.delete("/delete/:id",deleteUser);
 
 export default router;
